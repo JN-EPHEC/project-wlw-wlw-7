32d5ebb163c39a6790b60c72640f0b36e7206e69
imanebhjr <he135301@students.ephec.be>
2025-12-07 21:45:21 +0100

    jdcb

--- app/Profile/Fake_payment.tsx
+++ app/Profile/Fake_payment.tsx
@@ -73,7 +73,7 @@ export default function FakePaymentScreen() {
   const handleSuccessClose = () => {
     setShowSuccess(false);
     // Retourner au profil
-    router.replace("/(tabs)/Profile");
+    router.replace("/(tabs)/profile");
   };
 
   return (
